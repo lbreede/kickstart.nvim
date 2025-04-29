e4ea5411bbd98197f2b91bea27daa3bb1e510a45
lbreede <67719520+lbreede@users.noreply.github.com>
2025-04-28 21:55:31 -0700

    removed copilot and remapped confirm key

--- init.lua
+++ init.lua
@@ -250,6 +250,8 @@ require('lazy').setup({
   -- Use `opts = {}` to force a plugin to be loaded.
   --
   { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true, opts = ... },
+  -- { 'github/copilot.vim' },
+  -- { 'zbirenbaum/copilot.lua' },
 
   -- Rust-related plugins
   --
@@ -835,13 +837,13 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          -- ['<C-y>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
-          --['<CR>'] = cmp.mapping.confirm { select = true },
-          --['<Tab>'] = cmp.mapping.select_next_item(),
-          --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          ['<CR>'] = cmp.mapping.confirm { select = true },
+          ['<Tab>'] = cmp.mapping.select_next_item(),
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
